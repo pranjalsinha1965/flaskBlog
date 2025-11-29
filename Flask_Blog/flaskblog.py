c394eda3678fd33d184629d4f9126a433c4e2c40
pranjalsinha1965 <pranjalsinha1965@gmail.com>
2025-11-29 12:50:39 +0530

    Add flash messages, proper login form and check flaskblog.py

--- Flask_Blog/flaskblog.py
+++ Flask_Blog/flaskblog.py
@@ -1,7 +1,7 @@
-from flask import Flask, render_template, url_for 
+from flask import Flask, render_template, url_for, flash, redirect 
 from forms import RegistrationForm, LoginForm 
-app = Flask(__name__)
 
+app = Flask(__name__)
 app.config['SECRET_KEY'] = '7fdef6f33454ed00a6b93d7463cdb9aa'
 
 posts = [
@@ -22,21 +22,30 @@ posts = [
 @app.route("/")
 @app.route('/home')
 def home():
-    return render_template('home.html', posts = posts)
+    return render_template('home.html', posts=posts)
 
 @app.route('/about')
 def about():
-    return render_template('about.html')
+    return render_template('about.html', title="About")
 
-@app.route("/register")
+@app.route("/register", methods=['GET', 'POST'])
 def register():
     form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('home'))
     return render_template('register.html', title='Register', form=form)
 
-@app.route("/login")
+@app.route("/login", methods=['GET', 'POST'])
 def login():
     form = LoginForm()
-    return render_template('register.html', title='Register', form=form)
+    if form.validate_on_submit():
+        if form.email.data == 'admin@blog.com' and form.password.data == 'password':
+            flash('You have been logged in!', 'success')
+            return redirect(url_for('home'))
+        else: 
+            flash('Login Unsuccessful. Please check username and password', 'danger')
+    return render_template('login.html', title='Login', form=form)
 
 if __name__ == '__main__':
     app.run(debug=True)
--- Flask_Blog/forms.py
+++ Flask_Blog/forms.py
@@ -3,7 +3,7 @@ from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo
 
 class RegistrationForm(FlaskForm): 
-    username = StringField('Username', validators=[DataRequired()])
+    username = StringField('Username', validators=[DataRequired(), Length(min=2, max=20)])
     email = StringField('Email', validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[DataRequired()])
     confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
@@ -14,3 +14,5 @@ class LoginForm(FlaskForm):
     password = PasswordField('Password', validators=[DataRequired()])
     remember = BooleanField('Remember Me')
     submit = SubmitField('Login')
+
+
