e766ceb9d255044d7004c4a8f1210bea5db0fef9
pranjalsinha1965 <pranjalsinha1965@gmail.com>
2025-11-30 16:55:10 +0530

    Add user account access

--- Flask_Blog/flaskblog/__init__.py
+++ Flask_Blog/flaskblog/__init__.py
@@ -6,11 +6,18 @@ from flask_login import LoginManager
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '7fdef6f33454ed00a6b93d7463cdb9aa'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+
+# Template auto-reload FIX (must be AFTER app = Flask(__name__))
+app.config['TEMPLATES_AUTO_RELOAD'] = True
+app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
+app.jinja_env.auto_reload = True
+
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
+
 login_manager = LoginManager(app)
-login_manager.login_view = 'login'              # REQUIRED for login redirect
-login_manager.login_message_category = 'info'   # Flash message style
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'info'
 
 from flaskblog import routes
 
--- Flask_Blog/flaskblog/models.py
+++ Flask_Blog/flaskblog/models.py
@@ -4,7 +4,7 @@ from flask_login import UserMixin
 
 @login_manager.user_loader
 def load_user(user_id): 
-    return User.query.get(int())
+    return User.query.get(int(user_id))
 
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
