9515ec7405df6c9fcb638d58b7258dc2094e236a
pranjalsinha1965 <pranjalsinha1965@gmail.com>
2025-11-30 14:01:22 +0530

    Add user account authorization

--- Flask_Blog/flaskblog/__init__.py
+++ Flask_Blog/flaskblog/__init__.py
@@ -1,9 +1,16 @@
-from flask import Flask 
-from flask_sqlalchemy import SQLAlchemy 
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '7fdef6f33454ed00a6b93d7463cdb9aa'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'              # REQUIRED for login redirect
+login_manager.login_message_category = 'info'   # Flash message style
 
-from flaskblog import routes 
+from flaskblog import routes
+
